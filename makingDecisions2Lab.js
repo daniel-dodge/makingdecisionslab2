9e861f14fb3b951a54876d268c2569875758afbb
Daniel Dodge <dandantheman87@gmail.com>
2021-11-11 15:52:22 -0700

    done

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -49,8 +49,6 @@ for (let i = 0; i < nums.length; i++){
   console.log(nums[i])
   if ( nums[i]%2 === 0)
   evensArr.push(nums[i])
- 
-
 }
 console.log(evensArr)
 
@@ -137,7 +135,9 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+for (let i = letters.length-1; i > -1; i--){
+  console.log(letters[i])
+}
 
 ////////// Advanced Problems //////////
 
@@ -145,7 +145,7 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 // Switch statements can be excellent alternatives to if blocks. Look up switch statements (I recommend W3 Schools) and try to implement one for the following.
 
 // Do not edit the code below.
-let letterGrade = 'B'
+let letterGrade = "B"
 // Do not edit the code above.
 
 /* Use a switch statement on 'letterGrade' and console.log the appropriate response.
@@ -159,7 +159,18 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 */
 
 //Code Here
-
+switch (letterGrade){
+  case "A" : console.log("The student is doing excellently.");
+  break;
+  case "B" : console.log("The student is doing well.");
+  break;
+  case "C" : console.log("The student is doing alright.");
+  break;
+  case "D" : console.log("The student is not doing very well.");
+  break;
+  case "F" : console.log("The student is failing.");
+  break;
+}
 
 
 ////////// PROBLEM 11 //////////
@@ -187,3 +198,17 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 */
 
 //Code Here
+for (let i = 0; i < 100; i++){
+  
+  if ([i] % 5 === 0 && [i] % 3 === 0 ){
+    console.log("Devmountain")
+  }
+  else if ([i] % 3 === 0){ 
+console.log("Dev")
+  }else if ([i] % 5 === 0){
+    console.log("mountain")
+  } else if ( [i] % 5 !== 0 && [i] % 3 !== 0 ) {
+    console.log(i)
+  }
+  
+}
